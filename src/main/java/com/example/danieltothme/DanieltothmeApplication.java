02b50d68d042fa10f28c09b77bf632fb83a0b273
Your Name <tothd0504@gmail.com>
2025-05-17 23:19:01 +0200

    Basic Page With One Project

---
+++ src/main/java/com/example/danieltothme/DanieltothmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.danieltothme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanieltothmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanieltothmeApplication.class, args);
+	}
+
+}
